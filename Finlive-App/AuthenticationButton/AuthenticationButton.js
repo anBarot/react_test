8275a04dee03a60aea3113731275dc127f446162
anBarot <barot.anne@gmail.com>
2023-06-02 11:20:30 +0200

    adding function fingerPrint not available

--- Finlive-App/AuthenticationButton/AuthenticationButton.js
+++ Finlive-App/AuthenticationButton/AuthenticationButton.js
@@ -1,12 +1,28 @@
 import CustomButton from '../CustomButton';
 // import FingerprintScanner from 'react-native-fingerprint-scanner';
 import * as LocalAuthentication from 'expo-local-authentication';
-import React from 'react';
+import React, {useEffect} from 'react';
 
 export default function AuthenticationButton() {
+
+	useEffect(() => {
+		checkFingerprintCompatibility();
+	  }, []);
+	  
+	  const checkFingerprintCompatibility = async () => {
+		try {
+		  const isCompatible = await LocalAuthentication.hasHardwareAsync() && await LocalAuthentication.isEnrolledAsync();
+		  if (!isCompatible) {
+			alert('Fingerprint not available');
+		  }
+		} catch (error) {
+		  alert('Error');
+		}
+	  };
+
 	const handleAuthentication = async () => {
 		const { success } = await LocalAuthentication.authenticateAsync();
-	
+
 		if (success) {
 	  		alert('Authentication successful');
 		} else {
