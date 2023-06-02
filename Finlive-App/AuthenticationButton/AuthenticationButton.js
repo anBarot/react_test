72063912d4332450df064c97cc14bc5bd832cf71
anBarot <barot.anne@gmail.com>
2023-06-02 08:16:51 +0200

    new branch for authentication issue

--- Finlive-App/AuthenticationButton/AuthenticationButton.js
+++ Finlive-App/AuthenticationButton/AuthenticationButton.js
@@ -1,19 +1,30 @@
 import CustomButton from '../CustomButton';
+import FingerprintScanner from 'react-native-fingerprint-scanner';
 import * as LocalAuthentication from 'expo-local-authentication';
+import React, { useEffect } from 'react';
+import { Alert } from 'react-native';
+
+const authenticateWithFingerprint = async () => {
+	const hasHardware = await LocalAuthentication.hasHardwareAsync();
+	const hasEnrolledFingerprints = await LocalAuthentication.isEnrolledAsync();
+  
+	if (hasHardware && hasEnrolledFingerprints) {
+	  const { success, error } = await LocalAuthentication.authenticateAsync({
+		promptMessage: 'Scan your fingerprint',
+	  });
+	  if (success) {
+		Alert.alert('Success', 'Fingerprint authentication successful');
+	  } else {
+		Alert.alert('Error', 'Fingerprint authentication failed');
+	  }
+	} else {
+	  Alert.alert('Error', 'Fingerprint authentication is not available on this device');
+	}
+  };
 
 export default function AuthenticationButton() {
-	const handleAuthentication = async () => {
-		const { success, error } = await LocalAuthentication.authenticateAsync();
-
-		if (success) {
-		  alert('Authentication successful');
-		} else {
-		  alert('Authentication failed');
-		  console.log('Authentication error:', error);
-		}
-	  };
 	
 	  return (
-    	<CustomButton title="Authenticate" onPress={handleAuthentication} />
+    	<CustomButton title="Authenticate" onPress={authenticateWithFingerprint} />
 	);
 }
